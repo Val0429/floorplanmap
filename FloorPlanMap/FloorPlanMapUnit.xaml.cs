ab40a08282bc20866e13a15798ca6a8a4db6932a
Val <val.liu@isapsolution.com>
2018-01-26 15:50:44 +0800

    stable zoom

--- FloorPlanMap/FloorPlanMapUnit.xaml.cs
+++ FloorPlanMap/FloorPlanMapUnit.xaml.cs
@@ -20,6 +20,7 @@ using ObjectType = FloorPlanMap.Components.BaseComponent;
 //using ObjectCollection = System.Collections.ObjectModel.ObservableCollection<FloorPlanMap.Components.Objects.CameraObject>;
 using ObjectCollection = System.Windows.FreezableCollection<FloorPlanMap.Components.BaseComponent>;
 using System.Collections.Specialized;
+using System.Windows.Media.Animation;
 
 namespace FloorPlanMap
 {
@@ -33,12 +34,17 @@ namespace FloorPlanMap
             SetValue(ObjectsProperty, collection);
         }
 
+        #region "Mouse PTZ Function"
         private Point lastMousePos = new Point(0, 0);
         protected override void OnMouseWheel(MouseWheelEventArgs e) {
             Point position = e.GetPosition(this);
 
-            if (e.Delta > 0) ZoomScale *= 1.1;
-            if (e.Delta < 0 && ZoomScale > 1) ZoomScale /= 1.1;
+            if (e.Delta > 0) {
+                _zoomLevel = Math.Min(_zoomLevel + 1, MaxZoomLevel);
+            } else if (e.Delta < 0) {
+                _zoomLevel = Math.Max(_zoomLevel - 1, 1);
+            }
+            ZoomScale = Math.Pow(_zoomRatio, _zoomLevel-1);
 
             if (lastMousePos.X != position.X || lastMousePos.Y != position.Y) {
                 ScaleCenterX += (position.X - ScaleCenterX) / ZoomScale;
@@ -47,37 +53,48 @@ namespace FloorPlanMap
             }
         }
 
+        private bool dragging = false;
+        private Point? dragLastPosition = null;
+        protected override void OnMouseDown(MouseButtonEventArgs e) {
+            base.OnMouseDown(e);
+            dragLastPosition = e.GetPosition(this);
+            dragging = true;
+        }
+
+        protected override void OnMouseUp(MouseButtonEventArgs e) {
+            base.OnMouseUp(e);
+            dragging = false;
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e) {
+            base.OnMouseMove(e);
+            if (!dragging) return;
+            Point pos = e.GetPosition(this);
+
+            double scale = ZoomScale;
+
+            var scx = ScaleCenterX - (pos.X - dragLastPosition.Value.X) / scale;
+            scx = Math.Min(Math.Max(scx, 0), Border.ActualWidth);
+            var scy = ScaleCenterY - (pos.Y - dragLastPosition.Value.Y) / scale;
+            scy = Math.Min(Math.Max(scy, 0), Border.ActualHeight);
+
+            Console.WriteLine("{0} {1} {2}", scale, scx, pos.X);
+
+            dragLastPosition = pos;
+
+            Animation = false;
+            ScaleCenterX = scx;
+            ScaleCenterY = scy;
+            Animation = true;
+        }
+
+        protected override void OnMouseLeave(MouseEventArgs e) {
+            base.OnMouseLeave(e);
+            dragging = false;
+        }
+        #endregion "Mouse PTZ Function"
+
         #region "Dependency Properties"
-
-        #region "ZoomScale"
-        public static readonly DependencyProperty ZoomScaleProperty = DependencyProperty.Register(
-                "ZoomScale", typeof(double), typeof(FloorPlanMapUnit), new PropertyMetadata(1.0));
-        [Description("Zoom Scale."), Category("Source")]
-        public double ZoomScale {
-            get { return (double)GetValue(ZoomScaleProperty); }
-            set { SetValue(ZoomScaleProperty, value); }
-        }
-        #endregion "ZoomScale"
-
-        #region "ScaleCenterX"
-        public static readonly DependencyProperty ScaleCenterXProperty = DependencyProperty.Register(
-                "ScaleCenterX", typeof(double), typeof(FloorPlanMapUnit), new PropertyMetadata(0.0));
-        [Description("Scale Center X."), Category("Source")]
-        public double ScaleCenterX {
-            get { return (double)GetValue(ScaleCenterXProperty); }
-            set { SetValue(ScaleCenterXProperty, value); }
-        }
-        #endregion "ScaleCenterX"
-
-        #region "ScaleCenterY"
-        public static readonly DependencyProperty ScaleCenterYProperty = DependencyProperty.Register(
-                "ScaleCenterY", typeof(double), typeof(FloorPlanMapUnit), new PropertyMetadata(0.0));
-        [Description("Scale Center Y."), Category("Source")]
-        public double ScaleCenterY {
-            get { return (double)GetValue(ScaleCenterYProperty); }
-            set { SetValue(ScaleCenterYProperty, value); }
-        }
-        #endregion "ScaleCenterY"
 
         #region "MainSource"
         public static readonly DependencyProperty MapSourceProperty = DependencyProperty.Register(
@@ -115,7 +132,77 @@ namespace FloorPlanMap
         }
         #endregion "Objects"
 
+        #region "MaxZoomLevel"
+        private int _zoomLevel = 1;
+        private double _zoomRatio = 1.2;
+        public static readonly DependencyProperty MaxZoomLevelProperty = DependencyProperty.Register(
+                "MaxZoomLevel", typeof(int), typeof(FloorPlanMapUnit), new PropertyMetadata(12));
+        [Description("Max Zoom Level."), Category("Source")]
+        public int MaxZoomLevel {
+            get { return (int)GetValue(MaxZoomLevelProperty); }
+            set { SetValue(MaxZoomLevelProperty, value); }
+        }
+        #endregion "MaxZoomLevel"
+
         #endregion "Dependency Properties"
+
+        #region "Private Dependency Properties"
+        #region "ZoomScale"
+        private static readonly DependencyProperty ZoomScaleProperty = DependencyProperty.Register(
+                "ZoomScale", typeof(double), typeof(FloorPlanMapUnit), new PropertyMetadata(1.0));
+        [Description("Zoom Scale."), Category("Source")]
+        private double ZoomScale {
+            get { return (double)GetValue(ZoomScaleProperty); }
+            set { SetDispatcherAnimationValue<DoubleAnimation>(ZoomScaleProperty, value, 100); }
+        }
+        #endregion "ZoomScale"
+
+        #region "ScaleCenterX"
+        private static readonly DependencyProperty ScaleCenterXProperty = DependencyProperty.Register(
+                "ScaleCenterX", typeof(double), typeof(FloorPlanMapUnit), new PropertyMetadata(0.0));
+        [Description("Scale Center X."), Category("Source")]
+        private double ScaleCenterX {
+            get { return (double)GetValue(ScaleCenterXProperty); }
+            set { SetDispatcherAnimationValue<DoubleAnimation>(ScaleCenterXProperty, value, 100); }
+        }
+        #endregion "ScaleCenterX"
+
+        #region "ScaleCenterY"
+        private static readonly DependencyProperty ScaleCenterYProperty = DependencyProperty.Register(
+                "ScaleCenterY", typeof(double), typeof(FloorPlanMapUnit), new PropertyMetadata(0.0));
+        [Description("Scale Center Y."), Category("Source")]
+        private double ScaleCenterY {
+            get { return (double)GetValue(ScaleCenterYProperty); }
+            set { SetDispatcherAnimationValue<DoubleAnimation>(ScaleCenterYProperty, value, 100); }
+        }
+        #endregion "ScaleCenterY"
+
+        #region "Animation"
+        private static readonly DependencyProperty AnimationProperty = DependencyProperty.Register(
+                "Animation", typeof(bool), typeof(FloorPlanMapUnit),
+                new FrameworkPropertyMetadata(true));
+        [Description("Animation on / off."), Category("Source")]
+        private bool Animation {
+            get { return (bool)GetValue(AnimationProperty); }
+            set { SetValue(AnimationProperty, value); }
+        }
+        #endregion "Animation"
+
+        #endregion "Private Dependency Properties"
+
+        #region "Private Helper"
+        protected void SetDispatcherAnimationValue<T>(DependencyProperty dp, object value, double duration) where T : AnimationTimeline {
+            var rd = Animation ? duration : 0;
+            if (rd != 0) {
+                var instance = Activator.CreateInstance(typeof(T), new object[] { value, (Duration)TimeSpan.FromMilliseconds(rd) }) as T;
+                this.BeginAnimation(dp, null, HandoffBehavior.Compose);
+                this.BeginAnimation(dp, instance, HandoffBehavior.Compose);
+            } else {
+                this.BeginAnimation(dp, null);
+                SetValue(dp, value);
+            }
+        }
+        #endregion "Private Helper"
 
     }
 }
